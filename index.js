499c9e22a12c324f5aaf42253d408bf0e1c70da7
leenorris <lee.norris@philips.com>
2018-12-27 12:50:19 -0500

    removed-rxnode

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const rx = require('rx-node');
-const { spawn } = require('child_process');
+const {spawn} = require('child_process');
+const {timer, Observable, Subject, ReplaySubject, from, of, range} = require('rxjs');
+const {map, filter, switchMap, takeUntil} = require('rxjs/operators');
 
 const proc = spawn('sh', ['-c', 'ping www.google.com']);
 
-rx.fromStream(proc.stdout)
-    .map(buf => buf.toString())
-    .subscribe(console.log.bind(console));
+const ping = new Observable(o => {
+    proc.stdout.on('data', data => o.next(data));
+    proc.stdout.on('close', () => o.complete());
+})
+    ping.pipe(map(x => x.toString()), takeUntil(timer(5000)))
+    .subscribe(x => console.log(x), undefined, () => proc.kill());
